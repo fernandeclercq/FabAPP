0b8883c6762361398777500d4b428df854e37217
fernandeclercq <fernandomedinadb@hotmail.com>
2022-05-01 23:39:47 +0200

    refactoring convertgerbers

--- App/convertgerbers.py
+++ App/convertgerbers.py
@@ -1,4 +1,5 @@
 import os
+import platform
 
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
@@ -16,7 +17,7 @@ class ConvertGerbers(QDialog, Ui_Dialog):
         self.btnImportGrbs.clicked.connect(self.evt_btnImportGerbers_clicked)
         self.btnRenameGerbers.clicked.connect(self.evt_btnRenameGerbers_clicked)
 
-        self.myGerbersPath = os.getcwd()
+        self.myGerbersZipPath = os.getcwd()
 
 
         self.btnRenameGerbers.setEnabled(False)
@@ -37,12 +38,12 @@ class ConvertGerbers(QDialog, Ui_Dialog):
         event.accept()
         event.setDropAction(Qt.CopyAction)
         fileName = event.mimeData().urls()[0].fileName()
-        self.myGerbersPath = event.mimeData().urls()[0].toLocalFile()
+        self.myGerbersZipPath = event.mimeData().urls()[0].toLocalFile()
 
         self.gerbersAccepted(fileName)
 
 
-        with zipfile.ZipFile(str(self.myGerbersPath), 'r') as myGerbers:
+        with zipfile.ZipFile(str(self.myGerbersZipPath), 'r') as myGerbers:
             myPCBLayers = myGerbers.namelist()
             print(myPCBLayers)
 
@@ -55,31 +56,44 @@ class ConvertGerbers(QDialog, Ui_Dialog):
         pass
 
     def evt_btnImportGerbers_clicked(self):
-        self.myGerbersPath = QFileDialog.getOpenFileName(self, "Import Gerber Files",
-                                                         self.myGerbersPath, "Zip file(*.zip)")[0]
+        self.myGerbersZipPath = QFileDialog.getOpenFileName(self, "Import Gerber Files",
+                                                         self.myGerbersZipPath, "Zip file(*.zip)")[0]
 
-        #print(self.myGerbersPath)
-        with zipfile.ZipFile(str(self.myGerbersPath), 'r') as myGerbers:
-        #    print(myGerbers.namelist())
 
-            for file in myGerbers.namelist():
-                lastSlashIndex =  file.rfind("/")
-                if file[(lastSlashIndex + 1):] != "":
+        gerberAbsPaths = self.getGerbersFiles(self.myGerbersZipPath)
+        print(gerberAbsPaths)
 
-                    if file.endswith(".gbr"):
-                        print(file)
-                        with myGerbers.open(file, mode='r') as thefile:
-                            lstStr = []
-                            for x in range(0, 10):
-                                temp = thefile.readline(200)
-                                #print(temp)
-                                if temp != "b'G75*\\n'":
-                                    print(temp)
-                                #     lstStr.append(thefile.readline(200))
-                                # else:
-                                #     break;
-                                # print(lstStr)
 
+        # #print(self.myGerbersPath)
+        # with zipfile.ZipFile(str(self.myGerbersPath), 'r') as myGerbers:
+        # #    print(myGerbers.namelist())
+        #
+        #     for file in myGerbers.namelist():
+        #         lastSlashIndex =  file.rfind("/")
+        #         if file[(lastSlashIndex + 1):] != "":
+        #
+        #             if file.endswith(".gbr"):
+        #                 print(file)
+        #                 with myGerbers.open(file, mode='r') as thefile:
+        #                     lstStr = []
+        #                     for x in range(0, 10):
+        #                         temp = thefile.readline().decode('UTF-8')
+        #                         if temp.find("G75*") == -1:
+        #                             print(temp[:-2])
+        #                         else:
+        #                             break
+
+
+    def getGerbersFiles(self, gerber_files) -> list[str]:
+        listBuffer = []
+        with zipfile.ZipFile(str(gerber_files), 'r') as myGerberFiles:
+
+            for gerberFile in myGerberFiles.namelist():
+                currentFileName = self.getFileName(gerberFile)
+                if currentFileName.endswith(".gbr") or currentFileName.endswith(".xln"):
+                    listBuffer.append(os.getcwd() + self.getPlatformDirectorySlash() + gerberFile)
+
+        return listBuffer
 
     def gerbersAccepted(self, filename):
         self.ledGerberPath.setText(self.myGerbersPath)
@@ -87,6 +101,36 @@ class ConvertGerbers(QDialog, Ui_Dialog):
         self.lblConvertGerbers.setText(filename)
         self.btnRenameGerbers.setEnabled(True)
 
+    def getFileName(self, gerber_file) -> str:
+        if gerber_file != "":
+            if gerber_file.find("/"):
+                idx = gerber_file.rfind("/")
+                return gerber_file[(idx+1):]
+            elif gerber_file.find("\\"):
+                idx = gerber_file.rfind("\\")
+                return gerber_file[(idx+1):]
+            else:
+                return ""
+
+    def getFilePathRoot(self, gerber_file) -> str:
+        if gerber_file != "":
+            if gerber_file.find("/"):
+                idx = gerber_file.rfind("/")
+                return gerber_file[0:idx]
+            elif gerber_file.find("\\"):
+                idx = gerber_file.rfind("\\")
+                return gerber_file[0:idx]
+            else:
+                return ""
+
+    def getPlatformDirectorySlash(self) -> str:
+        system = platform.system()
+        if system == "Windows":
+            return "\\"
+        elif system == "Darwin":
+            return "/"
+        elif system == "Linux":
+            return "/"
 
 
 
